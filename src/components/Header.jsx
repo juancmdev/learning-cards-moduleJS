d49388de7799681e08bd73c8e6c616ee30b4d99c
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-05-15 17:20:51 -0500

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@
 export default function Header() {
   return (
     <div>
-      <h1 className="text-5xl bg-amber-200">Header</h1>
+      <h1 className="text-5xl bg-amber-200 w-full h-48 flex justify-center items-center">Header</h1>
     </div>
   )
 }
