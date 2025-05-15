3795ce07323cbf4794bf5a9b5259b6cf5e552aa0
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-05-15 17:42:01 -0500

    updated

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,9 @@
+const links = ["Verbo To Be", "Verbo To Have", "Pronombres Personales"];
 
 export default function Nav() {
   return (
-    <div>
-      
-    </div>
+    <nav className="bg-blue-500 flex items-center justify-around p-4">
+        {links.map((link, index) => <h3>{link}</h3>)}
+    </nav>
   )
 }
