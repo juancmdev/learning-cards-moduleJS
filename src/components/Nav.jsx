5184e856e18e0ffeedecb1c96d97aca641dc29b1
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-05-16 17:44:12 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,9 @@
-const links = ["Verbo To Be", "Verbo To Have", "Pronombres Personales"];
+const links = ["Inicio","Verbo To Be", "Verbo To Have", "Pronombres Personales"];
 
 export default function Nav() {
   return (
     <nav className="bg-blue-500 flex items-center justify-around p-4">
-        {links.map((link, index) => <h3>{link}</h3>)}
+        {links.map((link, index) => <button className="rounded cursor-pointer p-2 bg-amber-200">{link}</button>)}
     </nav>
   )
 }
